b29b82f51341b42bdb7528c3703eab5f631bc959
yibaebi <elliot.yibaebi@yahoo.com>
2023-01-30 03:25:21 +0100

    add new assets

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -2,5 +2,7 @@ import advisoryAndConsultingImage from './Advisory & Consulting services.png'
 import digitalForDevImage from './Digital for Development Programs.png'
 import incubatorAndAcceleratorImage from './Incubator & Accelerator Programs.png'
 import hubsAndSpacesImage from './Innovation Hubs & Spaces.png'
+import ubuluLogo from "./ubulu_logo.png"
+import circularLogo from "./circular_logo.png"
 
-export { advisoryAndConsultingImage, digitalForDevImage, incubatorAndAcceleratorImage, hubsAndSpacesImage }
+export { circularLogo,advisoryAndConsultingImage, digitalForDevImage, incubatorAndAcceleratorImage, hubsAndSpacesImage, ubuluLogo}
--- src/assets/svgs/index.js
+++ src/assets/svgs/index.js
@@ -1,3 +1,5 @@
 import { ReactComponent as EyeIcon } from './eye_icon.svg'
+import { ReactComponent as HamburgerMenu } from './hamburger_menu.svg'
+import { ReactComponent as CloseIcon } from './close_icon.svg'
 
-export { EyeIcon }
+export { HamburgerMenu, CloseIcon, EyeIcon }
